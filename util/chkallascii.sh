2aff0d9584162e24453ae3478e4a05ab9da3f2c1
Alexander Nicholi <me@nicholatian.com>
2021-06-18 17:12:28 -0400

    add more data types

--- util/chkallascii.sh
+++ util/chkallascii.sh
@@ -7,9 +7,12 @@ else
 	FIND='find';
 fi
 
-util/chkascii.py $(${FIND} data/ -type f -name '*.snip') \
-	$(${FIND} etc/ -type f) $(${FIND} src/ -type f) \
+util/chkascii.py $(${FIND} etc/ -type f) $(${FIND} src/ -type f) \
 	$(${FIND} util/ -type f -name '*.py') .clang-format \
 	$(${FIND} util/ -type f -name '*.sh') .gitattributes .gitignore COPYING \
 	Makefile $(${FIND} data/ -type f -name '*.scrip') \
+	$(${FIND} data/ -type f -name '*.snip') \
+	$(${FIND} data/ -type f -name '*.ini') \
+	$(${FIND} data/ -type f -name '*.owb') \
+	$(${FIND} data/ -type f -name '*.owm') \
 	$(${FIND} .vscode/ -type f) 3rdparty/README;
