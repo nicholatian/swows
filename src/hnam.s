fb1f497b91d2c9bb0c6ca435db2e74560f7915d1
Alexander Nicholi <me@nicholatian.com>
2021-07-09 02:01:29 -0400

    hnam is better in assembly

---
+++ src/hnam.s
@@ -0,0 +1,49 @@
+
+.globl names_f
+.globl names_m
+.globl egdataMaylene_snip
+.globl egdataBlendan_snip
+
+names_f:
+	.word egdataMaylene_snip
+	.word egdataMaylene_snip
+	.word egdataMaylene_snip
+	.word egdataMaylene_snip
+	.word egdataMaylene_snip
+	.word egdataMaylene_snip
+	.word egdataMaylene_snip
+	.word egdataMaylene_snip
+	.word egdataMaylene_snip
+	.word egdataMaylene_snip
+	.word egdataMaylene_snip
+	.word egdataMaylene_snip
+	.word egdataMaylene_snip
+	.word egdataMaylene_snip
+	.word egdataMaylene_snip
+	.word egdataMaylene_snip
+	.word egdataMaylene_snip
+	.word egdataMaylene_snip
+	.word egdataMaylene_snip
+	.word egdataMaylene_snip
+
+names_m:
+	.word egdataBlendan_snip
+	.word egdataBlendan_snip
+	.word egdataBlendan_snip
+	.word egdataBlendan_snip
+	.word egdataBlendan_snip
+	.word egdataBlendan_snip
+	.word egdataBlendan_snip
+	.word egdataBlendan_snip
+	.word egdataBlendan_snip
+	.word egdataBlendan_snip
+	.word egdataBlendan_snip
+	.word egdataBlendan_snip
+	.word egdataBlendan_snip
+	.word egdataBlendan_snip
+	.word egdataBlendan_snip
+	.word egdataBlendan_snip
+	.word egdataBlendan_snip
+	.word egdataBlendan_snip
+	.word egdataBlendan_snip
+	.word egdataBlendan_snip
