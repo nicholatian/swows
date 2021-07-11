433afd16ca4f95d9e1b17f0bde0582a7f865d581
Alexander Nicholi <me@nicholatian.com>
2021-07-11 16:35:22 -0400

    update utils

--- util/hackalias.sh
+++ util/hackalias.sh
@@ -4,8 +4,8 @@
 # these aliases help hasten the search for offsets of symbols and the
 # locations that hook into them.
 
-alias hgrep='arm-none-eabi-nm pokeemerald.elf | grep ';
-alias higrep='arm-none-eabi-nm pokeemerald.elf | grep -i ';
-alias romdump='xxd -u -ps -c 16 -g 1 pokeemerald.gba';
-alias romgrep='xxd -u -ps -c 16 -g 1 pokeemerald.gba | grep ';
-alias romigrep='xxd -u -ps -c 16 -g 1 pokeemerald.gba | grep -i ';
+alias hgrep='arm-none-eabi-nm 3rdparty/emer.elf | grep ';
+alias higrep='arm-none-eabi-nm 3rdparty/emer.elf | grep -i ';
+alias romdump='xxd -u -ps -c 16 -g 1 3rdparty/emer.gba';
+alias romgrep='xxd -u -ps -c 16 -g 1 3rdparty/emer.gba | grep ';
+alias romigrep='xxd -u -ps -c 16 -g 1 3rdparty/emer.gba | grep -i ';
--- util/rubify.sh
+++ util/rubify.sh
@@ -1,17 +1,27 @@
 #!/bin/sh
 # -*- coding: utf-8 -*-
 
-mapbin() {
-	util/mapbin2ow.py b \
-	etc/mapruby/data/layouts/$1/map.bin \
-	data/extern/map/$2.owb;
-	util/mapbin2ow.py m \
-		etc/mapruby/data/layouts/$1/map.bin \
-		data/extern/map/$2.owm;
-	util/mapbin2ow.py b \
-		etc/mapruby/data/layouts/$1/border.bin \
-		data/extern/map/$2.mapb;
+MAPRUBY=etc/mapruby;
+R_MAPDATA=${MAPRUBY}/data/layouts;
+R_BLOCKSET=${MAPRUBY}/data/tilesets;
+MAPDATA=data/extern/map;
+TILESET=data/extern/map/tileset;
+BLOCKSET=data/extern/map/blockset;
+
+blockset() {
+	cp ${R_BLOCKSET}/$1/metatiles.bin ${BLOCKSET}/$2.bset;
+	cp ${R_BLOCKSET}/$1/metatile_attributes.bin ${BLOCKSET}/$2.bsa;
+cp ${R_BLOCKSET}/$1/tiles.png ${TILESET}/$2.4tn.il.png;
 }
 
-mkdir -p data/extern/map;
-mapbin LittlerootTown littleroot;
+mapdata() {
+	cp ${R_MAPDATA}/$1/map.bin ${MAPDATA}/$2.map;
+	cp ${R_MAPDATA}/$1/border.bin ${MAPDATA}/$2.mapb;
+}
+
+mkdir -p ${TILESET};
+mkdir -p ${BLOCKSET};
+blockset primary/general outside;
+blockset secondary/petalburg petalburg;
+mapdata LittlerootTown littleroot;
+mapdata PetalburgCity petalburg;
