2b409239283878a99dbf8efe348d9bfac073316f
Alexander Nicholi <me@nicholatian.com>
2021-07-23 08:40:05 -0400

    update aliases for localised use

--- util/hackalias.sh
+++ util/hackalias.sh
@@ -6,6 +6,7 @@
 
 alias hgrep='arm-none-eabi-nm 3rdparty/emer.elf | grep ';
 alias higrep='arm-none-eabi-nm 3rdparty/emer.elf | grep -i ';
-alias romdump='xxd -u -ps -c 16 -g 1 3rdparty/emer.gba';
-alias romgrep='xxd -u -ps -c 16 -g 1 3rdparty/emer.gba | grep ';
-alias romigrep='xxd -u -ps -c 16 -g 1 3rdparty/emer.gba | grep -i ';
+alias hxdump='xxd -u -c 16 -g 1 ';
+alias romdump='xxd -u -c 16 -g 1 3rdparty/emer.gba';
+alias romgrep='xxd -u -c 16 -g 1 3rdparty/emer.gba | grep ';
+alias romigrep='xxd -u -c 16 -g 1 3rdparty/emer.gba | grep -i ';
