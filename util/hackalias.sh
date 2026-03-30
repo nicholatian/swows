b0edec8f29edf55213b3658f2c50b081b87db498
Alexander Nicholi <me@nicholatian.com>
2026-03-31 01:38:14 +0700

    space out to help dumb syntax highlighters

--- util/hackalias.sh
+++ util/hackalias.sh
@@ -5,8 +5,13 @@
 # locations that hook into them.
 
 alias hgrep='arm-none-eabi-nm 3rdparty/emer.elf | grep ';
+
 alias higrep='arm-none-eabi-nm 3rdparty/emer.elf | grep -i ';
+
 alias hxdump='xxd -u -c 16 -g 1 ';
+
 alias romdump='xxd -u -c 16 -g 1 3rdparty/emer.gba';
+
 alias romgrep='xxd -u -c 16 -g 1 3rdparty/emer.gba | grep ';
+
 alias romigrep='xxd -u -c 16 -g 1 3rdparty/emer.gba | grep -i ';
